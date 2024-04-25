12300c04881ca1801060cc63ea54edd0e2f0f606
phongphudev <phongphu1710@gmail.com>
2024-04-25 17:16:47 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     var listMenu = $("#main-menu li");
+//     listMenu.click(function(e){
+//         e.preventDefault();
+//         listMenu.removeClass('active');
+//         $(this).addClass('active');
+//     });
+// });
